83d5f3f8b7296602f1715eb60c6ed500701365b4
YZ <yz12g24@soton.ac.uk>
2026-05-13 01:30:42 +0100

    Update dashboard UI and patient filtering

--- app.py
+++ app.py
@@ -6,20 +6,25 @@ init_data()
 load_styles()
 
 # initialise page
-
 if "selected_patient" not in st.session_state:
     st.session_state.selected_patient = None
 
 if "page" not in st.session_state:
     st.session_state.page = "home"
 
-# open page
+# read page from URL only once, then clear it
+query_page = st.query_params.get("page")
 
+if query_page:
+    st.session_state.page = query_page
+    st.query_params.clear()
+
+# open page
 if st.session_state.page == "home":
     import page_home
     page_home.show()
 
-if st.session_state.page == "select":
+elif st.session_state.page == "select":
     import page_select
     page_select.show()
 
@@ -30,4 +35,3 @@ elif st.session_state.page == "patient":
 elif st.session_state.page == "report":
     import page_report
     page_report.show()
-    
